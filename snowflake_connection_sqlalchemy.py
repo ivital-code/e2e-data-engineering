579508efd59d93fb6e9dc5dcfefcc51a9f453ffa
ivital-code <54650571+ivital-code@users.noreply.github.com>
2026-08-02 22:22:15 -0600

    .

--- snowflake_connection_sqlalchemy.py
+++ snowflake_connection_sqlalchemy.py
@@ -47,7 +47,7 @@
 
 #or call direct
 #python snowflake_connection.py
-# cmd.exe /c "set SNOWFLAKE_USER=snf_srvacc&& set SNOWFLAKE_PASSWORD=Laspalmas25011965$&& set SNOWFLAKE_ACCOUNT=ohwwdyr-vk51395&& set SNOWFLAKE_WAREHOUSE=COMPUTE_WH&& set SNOWFLAKE_DATABASE=AIRBNB&& set SNOWFLAKE_SCHEMA=DEV&& set SNOWFLAKE_ROLE=MY_SERVICE_ROLE&& cd /d C:\Users\52332\e2e-data-engineering&& python snowflake_connection.py"
+# cmd.exe /c "set SNOWFLAKE_USER=snf_srvacc&& set SNOWFLAKE_PASSWORD=Laspalmas25011965$&& set SNOWFLAKE_ACCOUNT=ohwwdyr-vk51395&& set SNOWFLAKE_WAREHOUSE=COMPUTE_WH&& set SNOWFLAKE_DATABASE=AIRBNB&& set SNOWFLAKE_SCHEMA=DEV&& set SNOWFLAKE_ROLE=MY_SERVICE_ROLE&& cd /d C:\Users\52332\e2e-data-engineering&& python snowflake_connection_sqlalchemy.py"
 
 import os
 from typing import List, Optional
