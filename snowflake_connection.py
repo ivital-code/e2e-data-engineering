d43321894888feb37fedf01a5609763433786687
ivital-code <54650571+ivital-code@users.noreply.github.com>
2026-08-02 22:01:31 -0600

    snowflake connection setup

--- snowflake_connection.py
+++ snowflake_connection.py
@@ -9,6 +9,20 @@
 #$env:SNOWFLAKE_SCHEMA = "DEV"
 #$env:SNOWFLAKE_ROLE = "MY_SERVICE_ROLE"
 
+# snowflake connection setup
+# create rol MY_SERVICE_ROLE;
+#CREATE or replace USER snf_srvacc
+#  PASSWORD = 'Laspalmas25011965$'
+#  DEFAULT_ROLE = MY_SERVICE_ROLE
+#  DEFAULT_WAREHOUSE = COMPUTE_WH
+#  DEFAULT_NAMESPACE = my_database.public
+#  MUST_CHANGE_PASSWORD = FALSE;
+
+#GRANT ROLE MY_SERVICE_ROLE TO USER snf_srvacc;
+#GRANT ALL PRIVILEGES ON DATABASE AIRBNB TO ROLE MY_SERVICE_ROLE;
+#GRANT ALL PRIVILEGES ON schema DEV TO ROLE MY_SERVICE_ROLE;
+#GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA AIRBNB.DEV TO ROLE MY_SERVICE_ROLE;
+
 #or call direct
 #python snowflake_connection.py
 # cmd.exe /c "set SNOWFLAKE_USER=snf_srvacc&& set SNOWFLAKE_PASSWORD=Laspalmas25011965$&& set SNOWFLAKE_ACCOUNT=ohwwdyr-vk51395&& set SNOWFLAKE_WAREHOUSE=COMPUTE_WH&& set SNOWFLAKE_DATABASE=AIRBNB&& set SNOWFLAKE_SCHEMA=DEV&& set SNOWFLAKE_ROLE=MY_SERVICE_ROLE&& cd /d C:\Users\52332\e2e-data-engineering&& python snowflake_connection.py"
@@ -48,7 +62,8 @@ def run_query(query: str):
 
 
 def main():
-    sample_query = "SELECT current_version() AS version"
+    #sample_query = "SELECT current_version() AS version"
+    sample_query = "select REVIEWER_NAME,REVIEW_TEXT from AIRBNB.DEV.FCT_REVIEWS fetch first 3 rows only"; 
     try:
         rows = run_query(sample_query)
         print("Snowflake connection successful.")
