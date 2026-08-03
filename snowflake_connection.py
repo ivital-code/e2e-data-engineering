259061b4a25202d596938d7e34f03e9347a223d8
ivital-code <54650571+ivital-code@users.noreply.github.com>
2026-08-02 21:31:21 -0600

    .

--- snowflake_connection.py
+++ snowflake_connection.py
@@ -9,6 +9,7 @@
 #$env:SNOWFLAKE_SCHEMA = "DEV"
 #$env:SNOWFLAKE_ROLE = "MY_SERVICE_ROLE"
 
+#or call direct
 #python snowflake_connection.py
 # cmd.exe /c "set SNOWFLAKE_USER=snf_srvacc&& set SNOWFLAKE_PASSWORD=Laspalmas25011965$&& set SNOWFLAKE_ACCOUNT=ohwwdyr-vk51395&& set SNOWFLAKE_WAREHOUSE=COMPUTE_WH&& set SNOWFLAKE_DATABASE=AIRBNB&& set SNOWFLAKE_SCHEMA=DEV&& set SNOWFLAKE_ROLE=MY_SERVICE_ROLE&& cd /d C:\Users\52332\e2e-data-engineering&& python snowflake_connection.py"
 
